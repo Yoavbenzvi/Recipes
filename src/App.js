004dcb1f5fcfefdadc28e595508ac149009f1647
YoavBZ <yoav.benzvi@gmail.com>
2020-02-04 18:54:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const APP_Id = "cf1b4ee8";
+	const APP_Key = "4d84f047175dea8b86bc791ecaeb46c4";
+
+	const [recipes, setRecipes] = useState([]);
+	const [search, setSearch] = useState("");
+	const [query, setQuery] = useState("chicken")
+
+	useEffect(() => {
+		//getRecipes();
+	}, [query]);
+
+	const getRecipes = async () => {
+		const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_Id}&app_key=${APP_Key}`);
+		const data = await response.json();
+		setRecipes(data.hits);
+		console.log(data.hits);
+	}
+
+	const updateSearch = (e) => {
+		setSearch(e.target.value);
+	}
+
+	const getSearch = (e) => {
+		e.preventDefault();
+		setQuery(search);
+		setSearch("");
+	}
+
+	return(
+		<div className="App">
+			<form 
+				onSubmit={(e) => getSearch(e)}
+				className="search-form">
+				<input 
+					className="search-bar" 
+					type="text" 
+					value={search} 
+					onChange={(e) => updateSearch(e)}
+				/>
+				<button className="search-button" type="submit">Search</button>
+			</form>
+			{recipes.map((recipe, i) => (
+				<Recipe 
+					key={i}
+					title={recipe.recipe.label} 
+					calories={recipe.recipe.calories} 
+					image={recipe.recipe.image}
+				/>
+			))}
+		</div>
+	)
 }
 
 export default App;
